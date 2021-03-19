12470ce3f1d4708defc167af91b50124e0b987d0
Gavin <gaviner95@gmail.com>
2021-03-19 15:37:09 +0800

    Changed App.js text + included reference links in changelog file

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Running on Gavin_test_1 branch
-          Page running successfully
-          Deployed via npm run deploy command onto homepage properties
-        </p>
+        <p>Running on Gavin_test_1 branch</p>
+        <p>Page running successfully</p>
+        <p>Deployed via npm run deploy command onto homepage properties</p>
+        <p>test</p>
         <a
           className="App-link"
           href="https://reactjs.org"
